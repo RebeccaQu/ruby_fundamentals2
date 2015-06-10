b083b4426089c40788438e90db661e9f97f4c915
Rebecca Qu <beks.qu@gmail.com>
2015-06-10 14:37:31 -0400

    Second Celsius Calculator

--- exercise5.rb
+++ exercise5.rb
@@ -1,12 +1,21 @@
 # program that convert Fahrenheit to Celsius 
 
 
-def calculate_celsius(incoming_temp_f)
-	(incoming_temp_f - 32) * 5/9 
-	# puts "Temperature (C): #{result}"
+# def calculate_celsius(incoming_temp_f)
+# 	(incoming_temp_f - 32) * 5/9 
+# 	# puts "Temperature (C): #{result}"
+# end
+
+# puts "Temperature (F):"
+# temp_f = gets.chomp.to_i
+
+# puts "Temperature (C): #{calculate_celsius(temp_f)}"
+
+def celsius_calculator(fahrenheit)
+	(fahrenheit -32) * 5/9
 end
 
-puts "Temperature (F):"
-temp_f = gets.chomp.to_i
+puts "Enter temperature in Fahrenheit:"
+f_input = gets.chomp.to_i
 
-puts "Temperature (C): #{calculate_celsius(temp_f)}"
+puts "Temperature in Celsius: #{celsius_calculator(f_input)}"
