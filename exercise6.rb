590984247139ca859b9b76f79676934155395904
Rebecca Qu <beks.qu@gmail.com>
2015-06-10 14:59:25 -0400

    2. Added Rice

--- exercise6.rb
+++ exercise6.rb
@@ -3,8 +3,15 @@
 grocery_list = ["apples", "bananas", "carrots", "potatoes"]
 
 # List out grocery items with "*" in front of each item
-grocery_list.each do |list_num|
-	puts "* #{list_num}"
+# grocery_list.each do |list_n|
+# 	puts "* #{list_n}"
+# end
+
+# Add rice to list and output again 
+
+grocery_list << "rice"
+
+grocery_list.each do |list_n|
+	puts "* #{list_n}"
 end
 
-#
