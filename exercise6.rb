c1462e6e94aaa1972199d21d91e745c21794094c
Rebecca Qu <beks.qu@gmail.com>
2015-06-10 14:56:34 -0400

    1. Print out grocery list with * in front of each item

---
+++ exercise6.rb
@@ -0,0 +1,10 @@
+# Grocery List 
+
+grocery_list = ["apples", "bananas", "carrots", "potatoes"]
+
+# List out grocery items with "*" in front of each item
+grocery_list.each do |list_num|
+	puts "* #{list_num}"
+end
+
+#
