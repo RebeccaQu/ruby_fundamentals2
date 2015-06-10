d57ddebaa9681af3aff15aab786d5df4803259b4
Rebecca Qu <beks.qu@gmail.com>
2015-06-10 15:38:09 -0400

    2. Created method for listing groceries

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,7 @@
 # Grocery List 
 
+puts "My Grocery List:"
+
 grocery_list = ["apples", "bananas", "carrots", "potatoes"]
 
 # List out grocery items with "*" in front of each item
@@ -11,7 +13,15 @@ grocery_list = ["apples", "bananas", "carrots", "potatoes"]
 
 grocery_list << "rice"
 
-grocery_list.each do |list_n|
-	puts "* #{list_n}"
+# grocery_list.each do |list_n|
+# 	puts "* #{list_n}"
+# end
+
+def list_groceries(list)
+	list.each do |item|
+		puts "* #{item}"
+	end	
 end
+ list_groceries(grocery_list)
+
 
